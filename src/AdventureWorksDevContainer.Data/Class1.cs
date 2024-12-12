4f73becd5761744712f84f9c251793849d0c73df
Christopher Hair <mystikweb@live.ca>
2024-12-12 00:56:37 +0000

    Adding hosting extensions for database project

--- src/AdventureWorksDevContainer.Data/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace AdventureWorksDevContainer.Data;
-
-public class Class1
-{
-
-}
---
+++ src/AdventureWorksDevContainer.Data/HostingExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AdventureWorksDevContainer.Data;
+
+public static class HostingExtensions
+{
+    public static IServiceCollection AddDataServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AdventureWorks2022Context>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("Application"), configure =>
+                configure.UseNetTopologySuite()
+                    .UseHierarchyId()));
+
+        // services.AddScoped<IProductRepository, ProductRepository>();
+        // services.AddScoped<IPersonRepository, PersonRepository>();
+
+        return services;
+    }
+}
